255a46107809084bd0afc515448c7a416e56ae5f
Tyler Yates <tyates907@gmail.com>
2019-12-01 17:01:42 -0800

    display results of API call

--- src/main.js
+++ src/main.js
@@ -24,23 +24,44 @@ $(document).ready(function() {
   });
 
   const displayElements = (output) => { //display doctors
-    //add check if limited to 10 outputs
     const docOutput = $('#docOutput');
     docOutput.html('');
     for (let i = 0; i < output.data.length; i++) {
       //display doctor first names and last names
       docOutput.append(`
+        <u>Result ${i+1}</u>
+        <br>
         First Name: ${output.data[i].profile.first_name} <br>
         Last Name: ${output.data[i].profile.last_name} <br>
         <br>
         `);
-        //for each practice i
-        //if practice within search area === true
-        //print "practice #i"
-        //display address, phone number, website and whether or not the doctor is accepting new patients
-
+        for (let j = 0; j < output.data[i].practices.length; j++) {
+          if (output.data[i].practices[j].within_search_area === true) {
+            docOutput.append(`
+              Practice Title: ${output.data[i].practices[j].name} <br>
+              Address:
+              ${output.data[i].practices[j].visit_address.street}
+              ${output.data[i].practices[j].visit_address.city},
+              ${output.data[i].practices[j].visit_address.state}
+              ${output.data[i].practices[j].visit_address.zip} <br>
+              Phone Number: ${output.data[i].practices[j].phones[0].number} <br>
+              Website: ${output.data[i].practices[j].visit_address.website} <br>
+              <br>
+              `)
+              if (output.data[i].practices[j].accepts_new_patients) {
+                docOutput.append(`
+                  Accepting New Patients <br>
+                  <br>
+                  `)
+              } else {
+                docOutput.append(`
+                  Not accepting New Patients <br>
+                  <br>
+                  `)
+              }
+          }
+        }
       }
-
       console.log(output);
     };
 
